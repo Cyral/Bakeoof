d6ed4a0a3090ab8f2253a4b12cbbe82cce0ceefc
Heath <cyral@pyratron.com>
2018-09-30 16:36:56 -0400

    Change API url

--- ui/src/app/api.service.ts
+++ ui/src/app/api.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class ApiService {
-  private readonly url: string = "https://b2165353.ngrok.io";
+  private readonly url: string = "/api";
   constructor(private http: HttpClient) {
 
   }
