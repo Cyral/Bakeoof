a79e2678bc590f76a1d1685e6b9274f1ec48d549
Heath <cyral@pyratron.com>
2018-09-30 12:10:27 -0400

    UI header

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ButtonComponent} from "./button/button.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ui/src/app/button/button.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  template: `
+    <div>
+      <span class="loader" *ngIf="loading">
+        <i class="fa fa-circle-o-notch fa-spin" aria-hidden="true"></i>
+      </span>
+      <ng-content></ng-content>
+    </div>
+  `,
+  styleUrls: ['./button.component.styl']
+})
+export class ButtonComponent {
+  @Input()
+  public loading?: boolean = true;
+}
