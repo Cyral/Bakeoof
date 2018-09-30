1f3f02dce1dbaa93331ba610fef038f83ca10eb0
haydeninghem <43583120+haydeninghem@users.noreply.github.com>
2018-09-30 14:41:20 -0400

    Add files via upload

--- cake.go
+++ cake.go
@@ -3,23 +3,29 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
-func displaySteps(args []string ) {
 
-	fmt.Printf("In a %s size bowl, add baking powder, bla ","yo")
 
+func cookTime() int{
+	rand.Seed(time.Now().Unix())
+	return rand.Intn((30 - 20 ) + 30)
 }
 
-func main() {
+func coolTime() int{
+	rand.Seed(time.Now().Unix())
+	return rand.Intn((30 - 5 ) + 5)
+}
+
+func cookCake(ingridients [7]string ){
 	var size [3]string
 	var sizeNum int
 
 	sizeNum  = rand.Intn(3)
 
-	fmt.Printf("in a %s size bowl, add baking powder, flour, (ingrendients go here) \n",size[sizeNum])
-	fmt.Printf("add %d eggs and whisk all together\n",3)
-	fmt.Printf("put contents of bowl into  cake pan. Put directly into oven %d minutes.\n",30)
-	fmt.Printf("Let the cake pan cool for %d minutes.\n",20)
-
+	fmt.Printf("in a %s size bowl, add baking powder, flour, ",size[sizeNum])
+	fmt.Printf("add %d eggs and whisk all together\n",getEggs())
+	fmt.Printf("put contents of bowl into  cake pan. Put directly into oven %d minutes.\n",cookTime())
+	fmt.Printf("Let the cake pan cool for %d minutes.\n",coolTime())
 }
