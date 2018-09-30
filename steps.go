909b0919fe2561fa4fdffd04fbc51c60c8f0eae7
Anthony H <the7bunsoflife@gmail.com>
2018-09-30 15:12:13 -0400

    Add files via upload

--- steps.go
+++ steps.go
@@ -21,12 +21,11 @@ func coolTime() int{
 	return rand.Intn((30 - 5 ) + 5)
 }
 
-
 func getEggs() int{
 	return rand.Intn(4) + 1
 }
 func beatEggs() string{
-	eggStep := fmt.Sprintf("Beat %d eggs in a bowl \n", getEggs())
+	eggStep := fmt.Sprintf("Beat %d eggs in a %s sized bowl \n", getEggs(), bowlSize())
 	return eggStep
 }
 
@@ -55,3 +54,36 @@ func combineAll() string {
 	return combineStep
 }
 
+func cookAll() string{
+	cookStep := fmt.Sprintf("Bake for %d minutes", cookTime())
+	return cookStep
+}
+
+func coolLocation() string{
+	bowls := [2]string{"refrigerator", "freezer"}
+	return bowls[rand.Intn(2) - 1]
+}
+
+func coolAll() string{
+	coolStep := fmt.Sprintf("Cool for %d minutes in the %s", coolTime(), coolLocation())
+	return coolStep
+}
+
+
+
+func CookMuffin() []string {
+	steps := make([]string, 7)
+	steps[0] = preheat()
+	steps[1] = getMuffinTins()
+	steps[2] = beatEggs()
+	steps[3] = combineAll()
+	steps[4] = pourMuffin()
+	steps[5] = cookAll()
+	steps[6] = coolAll()
+
+	return steps
+}
+
+func main() {
+	fmt.Print(CookMuffin())
+}
